948ad3647be3495fde9b2773cb5a35cc372015e9
HananHIssa <hananissa2018@gmail.com>
2024-02-18 23:04:47 +0200

    f

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useState } from 'react'
+function App() {
+ useState();
+ let[counter,setCount]=useState(0);
+ const changeCounter = ()=>{
+ setCount(counter+1);}
+  
+ return (
+  <>
+  <button onClick={changeCounter}>Hanan Issa {counter}</button>
+  </>
+ );
+}
+
+export default App
+
